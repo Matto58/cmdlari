bf01b6e8c9ee66e4d6b77f5e7e77329744dfafa3
Matto58 <mattothepou@gmail.com>
2022-04-05 14:28:09 +0200

    Updated to 220405

--- CMDlari/Program.cs
+++ CMDlari/Program.cs
@@ -2,6 +2,7 @@
 {
     public class app
     {
+        public int sweet = 3;
         public static string avg2grade(string avg)
         {
             double d = double.Parse(avg);
@@ -55,23 +56,23 @@
                 grade = "Neznámá hodnota";
             }
 
-            return grade;
+            return "Známka: " + grade;
         }
-        public static string grades2avg(string n)
+        public static string grades2avg(string n, bool minus)
         {
             double average = 0;
             double n3 = 0;
             bool mistake = false;
             int n2 = int.Parse(n);
+            
             for (int i = 0; i < n2; i++)
             {
                 mistake = false;
-                Console.WriteLine("Zadejte známku " + (i + 1) + ", přidejte \",5\" pokud končí mínusem:");
+                Console.WriteLine("Zadejte známku " + (i + 1) + ":");
                 string grade = Console.ReadLine();
-                Console.WriteLine("Zadejte váhu pro známku " + (i + 1) + ":");
-                double weight = float.Parse(Console.ReadLine());
+                int grade2 = int.Parse(grade);
 
-                /* Old version:
+                /* First version with weight:
                    if (grade > 5 || grade < 1 || weight > 10 || weight < 1) {
                        Console.WriteLine("Zadejte prosím platnou známku (1-5) nebo váhu (1-10)");
                        i--;
@@ -80,53 +81,64 @@
                        n3 += weight;
                    }
                 */
-                // New version:
-                if (grade == "1")
+                // Latest version:
+                if (minus) 
                 {
-                    average += weight;
-                }
-                else if (grade == "1-")
-                {
-                    average += 1.5 * weight;
-                }
-                else if (grade == "2")
-                {
-                    average += 2 * weight;
-                }
-                else if (grade == "2-")
-                {
-                    average += 2.5 * weight;
-                }
-                else if (grade == "3")
-                {
-                    average += 3 * weight;
-                }
-                else if (grade == "3-")
-                {
-                    average += 3.5 * weight;
-                }
-                else if (grade == "4")
-                {
-                    average += 4 * weight;
-                }
-                else if (grade == "4-")
-                {
-                    average += 4.5 * weight;
-                }
-                else if (grade == "5")
-                {
-                    average += 5 * weight;
+                    if (grade == "1")
+                    {
+                        average += 1;
+                    }
+                    else if (grade == "1-")
+                    {
+                        average += 1.5;
+                    }
+                    else if (grade == "2")
+                    {
+                        average += 2;
+                    }
+                    else if (grade == "2-")
+                    {
+                        average += 2.5;
+                    }
+                    else if (grade == "3")
+                    {
+                        average += 3;
+                    }
+                    else if (grade == "3-")
+                    {
+                        average += 3.5;
+                    }
+                    else if (grade == "4")
+                    {
+                        average += 4;
+                    }
+                    else if (grade == "4-")
+                    {
+                        average += 4.5;
+                    }
+                    else if (grade == "5")
+                    {
+                        average += 5;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Zadejte prosím platnou známku (1-5).");
+                        i--;
+                        mistake = true;
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("Zadejte prosím platnou známku (1-5).");
-                    i--;
-                    mistake = true;
-                }
-
-                if (!mistake)
-                {
-                    n3 += weight;
+                    if (grade2 > 5 || grade2 < 1)
+                    {
+                        Console.WriteLine("Zadejte prosím platnou známku (1-5).");
+                        i--;
+                        mistake = true;
+                    }
+                    else
+                    {
+                        average += grade2;
+                    }
                 }
 
             }
@@ -145,49 +157,201 @@
         public static void Main(string[] args)
         {
             info.version iv = new info.version();
-            Console.WriteLine("Vítejte v CMDlářích");
-            Console.Write("Verze: ");
-            if (iv.dev)
+            app App = new app();
+            if (args.Length == 0)
             {
-                Console.Write("vývojářská ");
-            }
-            switch (iv.type)
-            {
-                case 0:
-                    Console.Write("alfa ");
-                    break;
-                case 1:
-                    Console.Write("beta ");
-                    break;
-                default:
-                    break;
-            }
-            Console.Write(iv.major + "." + iv.minor + "." + iv.patch + "\n");
-            Console.WriteLine("Sestavení " + iv.build + "\n");
-            if (args.Length < 2)
-            {
-                Console.WriteLine("Není zadáno dostatek argumentů");
-                return;
-            }
-            else if (args.Length > 2)
-            {
-                Console.WriteLine("Zadáno příliš mnoho argumentů");
-                return;
+                Console.WriteLine("CMDláři " + iv.id);
+
+                Console.WriteLine("Použití: cmdlari [jazyk] [příkaz] [parametry]");
+                Console.WriteLine("Jazyky: čeština, angličtina, němčina");
+                Console.WriteLine("Příkazy:\nprůměr na známku (-pz/--prumer-na-znamku)\nznámky na průměr (-zp/--znamky-na-prumer)\nznámky na průměr s mínusem (-zpm/--znamky-na-prumer-s-minusem)\n");
+                Console.WriteLine("Parametry:\n[průměr známek] (-pz/--prumer-na-znamku)\n[počet známek] (-zp/--znamky-na-prumer/--zpm/--znamky-na-prumer-s-minusem)\n");
+
+                Console.WriteLine("Usage: cmdlari [command] [parameters]");
+                Console.WriteLine("Languages: Czech, English, German");
+                Console.WriteLine("Commands:\naverage to grade (-pz/--prumer-na-znamku)\ngrades to average (-zp/--znamky-na-prumer)\ngrades to average with minus (-zpm/--znamky-na-prumer-s-minusem)\n");
+                Console.WriteLine("Parameters:\n[average grade] (-pz/--prumer-na-znamku)\n[number of grades] (-zp/--znamky-na-prumer/--zpm/--znamky-na-prumer-s-minusem)\n");
+                
+                Console.WriteLine("Verwendung: cmdlari [Befehl] [Parameter]");
+                Console.WriteLine("Sprache: Tschechisch, Englisch, Deutsch");
+                Console.WriteLine("Befehle:\nDurchschnitt zu Note (-pz/--prumer-na-znamku)\nNoten zu Durchschnitt (-zp/--znamky-na-prumer)\nNoten zu Durchschnitt mit Minus (-zpm/--znamky-na-prumer-s-minusem)\n");
+                Console.WriteLine("Parameter:\n[Durchschnitt Note] (-pz/--prumer-na-znamku)\n[Anzahl Noten] (-zp/--znamky-na-prumer/--zpm/--znamky-na-prumer-s-minusem)\n");
             }
             else
             {
-                if (args[0] == "--prumer-na-znamku" || args[0] == "-pz")
+                if (args[0] == "-cz")
                 {
-                    Console.WriteLine(avg2grade(args[1]));
+                    Console.Write("Vítejte v CMDlářích, ");
+                    if (iv.dev)
+                    {
+                        Console.Write("vývojářská ");
+                    }
+                    switch (iv.type)
+                    {
+                        case 0:
+                            Console.Write("alfa ");
+                            break;
+                        case 1:
+                            Console.Write("beta ");
+                            break;
+                        case 2:
+                            Console.Write("verze ");
+                            break;
+                    }
+                    Console.Write(iv.major + "." + iv.minor + "." + iv.patch + "\n");
+                    Console.WriteLine("Sestavení " + iv.build + ", identifikátor " + iv.id + "\n");
+                    if (args.Length == 0)
+                    {
+                        Console.WriteLine("Použití: cmdlari [příkaz] [parametry]");
+                        Console.WriteLine("Příkazy:\nprůměr na známku (-pz/--prumer-na-znamku)\nznámky na průměr (-zp/--znamky-na-prumer)\nznámky na průměr s mínusem (-zpm/--znamky-na-prumer-s-minusem)\n");
+                        Console.WriteLine("Parametry:\n[průměr známek] (-pz/--prumer-na-znamku)\n[počet známek] (-zp/--znamky-na-prumer/--zpm/--znamky-na-prumer-s-minusem)\n");
+
+                    }
+                    else if (args.Length < App.sweet && args.Length != 0)
+                    {
+                        Console.WriteLine("Není zadáno dostatek argumentů");
+                        return;
+                    }
+                    else if (args.Length > App.sweet)
+                    {
+                        Console.WriteLine("Zadáno příliš mnoho argumentů");
+                        return;
+                    }
+                    else
+                    {
+                        if (args[1] == "--prumer-na-znamku" || args[1] == "-pz")
+                        {
+                            Console.WriteLine(avg2grade(args[2]));
+                        }
+                        else if (args[1] == "--znamky-na-prumer" || args[1] == "-zp")
+                        {
+                            Console.WriteLine(grades2avg(args[2], false));
+                        }
+                        else if (args[1] == "--znamky-na-prumer-s-minusem" || args[1] == "-zpm") {
+                            Console.WriteLine(grades2avg(args[2], true));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Neznámý argument");
+                        }
+                    } 
                 }
-                else if (args[0] == "--znamky-na-prumer" || args[0] == "-zp")
+                else if (args[0] == "-en")
                 {
-                    Console.WriteLine(grades2avg(args[1]));
+                    Console.Write("Welcome to CMDlari, ");
+                    if (iv.dev)
+                    {
+                        Console.Write("development ");
+                    }
+                    switch (iv.type)
+                    {
+                        case 0:
+                            Console.Write("alpha ");
+                            break;
+                        case 1:
+                            Console.Write("beta ");
+                            break;
+                        case 2:
+                            Console.Write("version ");
+                            break;
+                    }
+                    Console.Write(iv.major + "." + iv.minor + "." + iv.patch + "\n");
+                    Console.WriteLine("Build " + iv.build + ", identifier " + iv.id + "\n");
+                    if (args.Length == 0)
+                    {
+                        Console.WriteLine("Usage: cmdlari [command] [parameters]");
+                        Console.WriteLine("Commands:\naverage to grade (-pz/--prumer-na-znamku)\ngrades to average (-zp/--znamky-na-prumer)\ngrades to average with minus (-zpm/--znamky-na-prumer-s-minusem)\n");
+                        Console.WriteLine("Parameters:\n[average grade] (-pz/--prumer-na-znamku)\n[number of grades] (-zp/--znamky-na-prumer/--zpm/--znamky-na-prumer-s-minusem)\n");
+
+                    }
+                    else if (args.Length < App.sweet && args.Length != 0)
+                    {
+                        Console.WriteLine("Not enough arguments");
+                        return;
+                    }
+                    else if (args.Length > App.sweet)
+                    {
+                        Console.WriteLine("Too many arguments");
+                        return;
+                    }
+                    else
+                    {
+                        if (args[1] == "--prumer-na-znamku" || args[1] == "-pz")
+                        {
+                            Console.WriteLine(avg2grade(args[2]));
+                        }
+                        else if (args[1] == "--znamky-na-prumer" || args[1] == "-zp")
+                        {
+                            Console.WriteLine(grades2avg(args[2], false));
+                        }
+                        else if (args[1] == "--znamky-na-prumer-s-minusem" || args[1] == "-zpm")
+                        {
+                            Console.WriteLine(grades2avg(args[2], true));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Unknown argument");
+                        }
+                    }
                 }
-                else
+                else if (args[0] == "-de")
                 {
-                    Console.WriteLine("Neznámý argument");
-                }
+                    Console.Write("Willkommen in CMDlari, ");
+                    if (iv.dev)
+                    {
+                        Console.Write("Entwickler ");
+                    }
+                    switch (iv.type)
+                    {
+                        case 0:
+                            Console.Write("alpha ");
+                            break;
+                        case 1:
+                            Console.Write("beta ");
+                            break;
+                        case 2:
+                            Console.Write("Version ");
+                            break;
+                    }
+                    Console.Write(iv.major + "." + iv.minor + "." + iv.patch + "\n");
+                    Console.WriteLine("Build " + iv.build + ", Identifier " + iv.id + "\n");
+                    if (args.Length == 0)
+                    {
+                        Console.WriteLine("Verwendung: cmdlari [Befehl] [Parameter]");
+                        Console.WriteLine("Befehle:\nDurchschnitt zu Note (-pz/--prumer-na-znamku)\nNoten zu Durchschnitt (-zp/--znamky-na-prumer)\nNoten zu Durchschnitt mit Minus (-zpm/--znamky-na-prumer-s-minusem)\n");
+                        Console.WriteLine("Parameter:\n[Durchschnitt Note] (-pz/--prumer-na-znamku)\n[Anzahl Noten] (-zp/--znamky-na-prumer/--zpm/--znamky-na-prumer-s-minusem)\n");
+
+                    }
+                    else if (args.Length < App.sweet && args.Length != 0)
+                    {
+                        Console.WriteLine("Nicht genug Argumente");
+                        return;
+                    }
+                    else if (args.Length > App.sweet)
+                    {
+                        Console.WriteLine("Zu viele Argumente");
+                        return;
+                    }
+                    else
+                    {
+                        if (args[1] == "--prumer-na-znamku" || args[1] == "-pz")
+                        {
+                            Console.WriteLine(avg2grade(args[2]));
+                        }
+                        else if (args[1] == "--znamky-na-prumer" || args[1] == "-zp")
+                        {
+                            Console.WriteLine(grades2avg(args[2], false));
+                        }
+                        else if (args[1] == "--znamky-na-prumer-s-minusem" || args[1] == "-zpm")
+                        {
+                            Console.WriteLine(grades2avg(args[2], true));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Unbekannter Befehl");
+                        }
+                    }
+                }  
             }
         }
     }
@@ -202,8 +366,50 @@
             public string codename = "Delfín";
             public int major = 0;
             public int minor = 1;
-            public int patch = 0;
-            public int build = 1;
+            public int patch = 1;
+            public int build = 15;
+            public int id = 220405;
+        }
+    }
+    public class lang {
+        public class CZ_cs {
+            public string welcome = "Vítejte v CMDlářích, ";
+            public string build = "Sestavení ";
+            public string alpha = "alfa ";
+            public string beta = "beta ";
+            public string release = "verze ";
+            public string dev = "vývojářská ";
+            public string argfew = "Není zadáno dostatek argumentů";
+            public string argmany = "Zadáno příliš mnoho argumentů";
+            public string argunk = "Neznámý argument";
+            public string cmds = "Příkazy:\nprůměr na známku (-pz/--prumer-na-znamku)\nznámky na průměr (-zp/--znamky-na-prumer)\nznámky na průměr s mínusem (-zpm/--znamky-na-prumer-s-minusem)\n";
+            public string parameters = "Parametry:\n[průměr známek] (-pz/--prumer-na-znamku)\n[počet známek] (-zp/--znamky-na-prumer/--zpm/--znamky-na-prumer-s-minusem)\n";
+        }
+        public class US_en {
+            public string welcome = "Welcome to CMDláři, ";
+            public string build = "Build ";
+            public string alpha = "alpha ";
+            public string beta = "beta ";
+            public string release = "version ";
+            public string dev = "developer ";
+            public string argfew = "Not enough arguments";
+            public string argmany = "Too many arguments";
+            public string argunk = "Unknown argument";
+            public string cmds = "Commands:\naverage to grade (-pz/--prumer-na-znamku)\ngrades to average (-zp/--znamky-na-prumer)\ngrades to average with minus (-zpm/--znamky-na-prumer-s-minusem)\n";
+            public string parameters = "Parameters:\n[average grade] (-pz/--prumer-na-znamku)\n[number of grades] (-zp/--znamky-na-prumer/--zpm/--znamky-na-prumer-s-minusem)\n";
+        }
+        public class DE_de {
+            public string welcome = "Willkommen in CMDlari, ";
+            public string build = "Build ";
+            public string alpha = "alpha ";
+            public string beta = "beta ";
+            public string release = "version ";
+            public string dev = "Entwickler ";
+            public string argfew = "Nicht genug Argumente";
+            public string argmany = "Zu viele Argumente";
+            public string argunk = "Unbekanntes Argument";
+            public string cmds = "Befehle:\nDurchschnitt zu Note (-pz/--prumer-na-znamku)\nNoten zu Durchschnitt (-zp/--znamky-na-prumer)\nNoten zu Durchschnitt mit Minus (-zpm/--znamky-na-prumer-s-minusem)\n";
+            public string parameters = "Parameter:\n[Durchschnitt Note] (-pz/--prumer-na-znamku)\n[Anzahl Noten] (-zp/--znamky-na-prumer/--zpm/--znamky-na-prumer-s-minusem)\n";
         }
     }
 }
